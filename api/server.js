4580bcad1fb6ba60a54628f57e083efc184d9a10
Anjali <anjalimandal5701@gmail.com>
2025-03-30 00:00:10 +0530

    connect to db

--- api/server.js
+++ api/server.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// console.log('Mongo URI:', process.env.MONGO);  // Check if this prints the correct Mongo URI
+
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log('MongoDB connected');
+    })
+    .catch((error) => {
+        console.error('MongoDB connection error:', error);
+    });
+
 const app = express();
 
 app.listen(3000, () => {
